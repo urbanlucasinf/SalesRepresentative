31af1ca5c68a250733efb302ec13bfb7db902a24
urban <urban@DESKTOP-2P99BRB>
2017-03-09 15:53:35 +0100

    Add project files.

---
+++ SalesRepresentative/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SalesRepresentative.MvcApplication" Language="C#" %>
---
+++ SalesRepresentative/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SalesRepresentative.Startup))]
+namespace SalesRepresentative
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
